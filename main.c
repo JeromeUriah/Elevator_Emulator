fe4d3526764e9e03d4d5976f14ae188f74b1b394
JeromeUriah <jeromeuriahjiajun.ng@student.uq.edu.au>
2025-05-13 11:55:13 +1000

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * Assignment2.c
+ *
+ * Created: 12/5/2025 9:12:59 pm
+ * Author : jjunn
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
